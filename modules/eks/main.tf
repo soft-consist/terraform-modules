f95483eb7615bc42b4a71746a91278883347fd2d
ashubambal <ashutoshbambal97@gmail.com>
2026-01-15 19:57:45 +0530

    Adding sg inbound

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -88,3 +88,14 @@ resource "aws_eks_node_group" "this" {
     var.tags,
   )
 }
+
+resource "aws_security_group_rule" "alloow_private_endpoint_access" {
+  count               = length(var.allowd_cidr_blocks) > 0 ? 1: 0
+  type                = "ingress"
+  from_port           = 443
+  to_port             = 443
+  protocol            = "tcp"
+  security_group_id   = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+  cidr_blocks        = [var.allowd_cidr_blocks[count.index]]
+  description         = "Allow access to EKS cluster endpoint from allowed CIDR blocks"
+}

d6f2454471cde564d9b683e36faa5bbcd1fd1895
ashubambal <ashutoshbambal97@gmail.com>
2026-01-15 15:22:46 +0530

    adding nodgroup

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -65,3 +65,26 @@ resource "aws_iam_role_policy_attachment" "ecr_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+# Managed Node Group
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.env}-eks-node-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = var.private_subnet_ids  
+
+  scaling_config {
+    desired_size = var.desired_size
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  instance_types = var.node_instance_types
+  capacity_type = "ON_DEMAND"
+
+  tags = merge(
+    {
+      Name = "${var.env}-eks-node-group"
+    },
+    var.tags,
+  )
+}

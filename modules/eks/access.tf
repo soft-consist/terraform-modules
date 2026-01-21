4bad91766735a808f8ad88399d8aab173862f859
ashubambal <ashutoshbambal97@gmail.com>
2026-01-21 22:11:21 +0530

    aws-8

--- modules/eks/access.tf
+++ modules/eks/access.tf
@@ -16,17 +16,16 @@
 
 resource "aws_eks_access_entry" "bastion" {
   cluster_name  = aws_eks_cluster.this.name
-  principal_arn = aws_iam_role.bastion_access_role.arn
+  principal_arn = var.bastion_access_role_arn
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "bastion_admin" {
   cluster_name  = aws_eks_cluster.this.name
-  principal_arn = aws_iam_role.bastion_access_role.arn
+  principal_arn = var.bastion_access_role_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type = "cluster"
   }
 }
-

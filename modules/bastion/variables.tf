fae9999d9e30478787c98325ef20615d790a59b9
ashubambal <ashutoshbambal97@gmail.com>
2026-01-21 21:51:14 +0530

    aws-8

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -2,31 +2,35 @@ variable "env" {
   type = string
 }
 
-variable "vpc_id" {
-  type = string
-}
+# variable "vpc_id" {
+#   type = string
+# }
 
-variable "public_subnet_ids" {
-  type = list(string)
-}
+# variable "public_subnet_ids" {
+#   type = list(string)
+# }
 
-variable "ami" {
-  type = string
-}
+# variable "ami" {
+#   type = string
+# }
 
-variable "instance_type" {
-  type = string
-}
+# variable "instance_type" {
+#   type = string
+# }
 
-variable "key_name" {
-  type = string
-}
+# variable "key_name" {
+#   type = string
+# }
 
-variable "allowed_ssh_cidr" {
-  type = list(string)
-}
+# variable "allowed_ssh_cidr" {
+#   type = list(string)
+# }
 
 variable "tags" {
   type = map(string)
 }
 
+variable "bastion_assume_role_principals" {
+  description = "IAM principals allowed to assume the bastion access role"
+  type        = list(string)
+}

9130cf3ed0022aea8a43c4943df649d5419d2149
ashubambal <ashutoshbambal97@gmail.com>
2026-01-21 14:52:51 +0530

    Testing commit

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -1,3 +1,8 @@
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
+
+output "role_arn" {
+  description = "IAM role ARN for bastion host"
+  value       = aws_iam_role.this.arn
+}

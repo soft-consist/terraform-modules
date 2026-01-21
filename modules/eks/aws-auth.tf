585e783c2091404490ad97c27b18a80d98f7164b
ashubambal <ashutoshbambal97@gmail.com>
2026-01-21 11:55:55 +0530

    aws-1

---
+++ modules/eks/aws-auth.tf
@@ -0,0 +1,10 @@
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode(var.aws_auth_roles)
+  }
+}

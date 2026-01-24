7c609224d2d8b5636a272a4a17e916c8cd0ca9e9
ashubambal <ashutoshbambal97@gmail.com>
2026-01-24 23:47:03 +0530

    add-ons

--- modules/addons/main.tf
+++ modules/addons/main.tf
@@ -1,17 +1,10 @@
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name    = var.cluster_name
-  addon_name      = "kube-proxy"
-  addon_version   = var.kube_proxy_version
-}
+resource "aws_eks_addon" "this" {
+  for_each = var.addons
 
-resource "aws_eks_addon" "coredns" {
-  cluster_name    = var.cluster_name
-  addon_name      = "coredns"
-  addon_version   = var.coredns_version
-}
+  cluster_name  = var.cluster_name
+  addon_name    = each.key
+  addon_version = try(each.value.version, null)
 
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name    = var.cluster_name
-  addon_name      = "vpc-cni"
-  addon_version   = var.vpc_cni_version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
--- modules/addons/variables.tf
+++ modules/addons/variables.tf
@@ -3,17 +3,9 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "kube_proxy_version" {
-  description = "Version of kube-proxy to install"
-  type        = string
-}
-
-variable "coredns_version" {
-  description = "Version of CoreDNS to install"
-  type        = string
-}
-
-variable "vpc_cni_version" {
-  description = "Version of VPC CNI to install"
-  type        = string
+variable "addons" {
+  description = "Map of EKS addons to enable"
+  type = map(object({
+    version = optional(string)
+  }))
 }

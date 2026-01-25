5019e92696944e3b00a7b35f6f7ae5678f641f60
ashubambal <ashutoshbambal97@gmail.com>
2026-01-25 12:22:56 +0530

    testing irsa module

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -103,3 +103,13 @@ resource "aws_security_group_rule" "alloow_private_endpoint_access" {
   cidr_blocks        = [var.allowd_cidr_blocks[count.index]]
   description         = "Allow access to EKS cluster endpoint from allowed CIDR blocks"
 }
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  url             = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+}
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -12,3 +12,11 @@ output "cluster_ca" {
   description = "EKS Cluster CA"
   value       = aws_eks_cluster.this.certificate_authority[0].data
 }
+
+output "oidc_provider_arn" {
+  value = aws_eks_cluster.this.oidc.issuer_url
+}
+
+output "oidc_provider_url" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}

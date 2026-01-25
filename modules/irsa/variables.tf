ed5ddc7af3682e19af126a4f8bb1da5ef13d21e6
ashubambal <ashutoshbambal97@gmail.com>
2026-01-25 11:52:14 +0530

    testing irsa module

---
+++ modules/irsa/variables.tf
@@ -0,0 +1,27 @@
+variable "role_name" {
+  type = string
+}
+
+variable "oidc_provider_arn" {
+  type = string
+}
+
+variable "oidc_provider_url" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "service_account_name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "policy_arns" {
+  type = list(string)
+}

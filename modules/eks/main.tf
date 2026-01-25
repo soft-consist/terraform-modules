f417fb23c2e533f1ae56f1d73923e5be1c3b003c
ashubambal <ashutoshbambal97@gmail.com>
2026-01-25 13:21:11 +0530

    testing irsa module

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -109,7 +109,9 @@ data "tls_certificate" "eks" {
 }
 
 resource "aws_iam_openid_connect_provider" "this" {
-  url             = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  thumbprint_list = [
+    data.tls_certificate.eks.certificates[0].sha1_fingerprint
+  ]
 }

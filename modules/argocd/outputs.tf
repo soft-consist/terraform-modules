a48a92c23ec987bb89847254e35709f28ed158bc
ashubambal <ashutoshbambal97@gmail.com>
2026-01-26 14:53:12 +0530

    argocd

--- modules/argocd/outputs.tf
+++ modules/argocd/outputs.tf
@@ -1,8 +1,3 @@
 output "argocd_namespace" {
   value = kubernetes_namespace.argocd.metadata[0].name
 }
-
-output "cluster_certificate_authority_data" {
-  description = "EKS cluster CA data"
-  value       = aws_eks_cluster.this.certificate_authority[0].data
-}
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -20,3 +20,7 @@ output "oidc_provider_arn" {
 output "oidc_provider_url" {
   value = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
